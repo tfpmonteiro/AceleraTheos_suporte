1f717d091b96faeb7b942be7aababf538d2bdef1
Talita <talis_phn2@hotmail.com>
2018-10-15 14:04:09 -0300

    Resolução do ex. 03 e inclusao do git ignore

---
+++ Teste/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o nome do funcionário?");
+            string funcionario = Console.ReadLine();
+
+            Console.WriteLine($"Qual é o salário atual do(a) {funcionario}");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            double ajuste = (salario * 5) / 100;
+            salario = salario + ajuste;
+
+            Console.WriteLine($"O salário do(a) {funcionario} recebeu um ajuste de {ajuste.ToString("c")} e agora corresponde a {salario.ToString("c")}");
+            Console.ReadKey();
+        }
+    }
+}
